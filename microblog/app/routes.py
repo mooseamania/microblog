41ee5c30de74b4a05b8d4c5ac5f85b168a79de18
Seamus Owens <seamus.owens@gmail.com>
2019-02-26 06:48:44 -0500

    Update routes.py

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -24,5 +24,5 @@ def login():
 	form = LoginForm()
 	if form.validate_on_submit():
 		flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
-		return redirect('/index.html')
+		return redirect('/index')
 	return render_template('login.html', title='Sign In', form=form)
