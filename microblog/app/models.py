a3c3b92d1c60d54c6139047c9a75ec8e513137dc
Seamus Owens <seamus.owens@gmail.com>
2019-02-27 08:20:52 -0500

    Update models.py

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -17,4 +17,4 @@ class Post(db.Model):
 	timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 	def __repr__(self):
-		<return '<Post {}>'.format(self.body)
+		return '<Post {}>'.format(self.body)
